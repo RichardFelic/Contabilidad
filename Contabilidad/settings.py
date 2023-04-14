73b33dedbeed9895b7472b178d5788bd7d67d520
RichardFelic <felricharde42@gmail.com>
2023-04-14 14:17:16 -0400

    fdf

--- Contabilidad/settings.py
+++ Contabilidad/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [BASE_DIR / "static"]sss
 # Default primary key field type
